a395f30d82dbc9a38dbd63e71a8dfd177c3081c8
MonicaTifaniZ <43172036+MonicaTifaniZ@users.noreply.github.com>
2019-12-08 16:01:58 +0700

    new commit

---
+++ Digimagz/app/src/main/java/com/monicatifanyz/digimagz/api/ApiClient.kt
@@ -0,0 +1,9 @@
+package com.monicatifanyz.digimagz.api
+
+import retrofit2.Retrofit
+
+class ApiClient {
+    private val retrofit: Retrofit? = null
+    private val httpLoggingInterceptor:HttpLoggingInterceptor
+
+}
---
+++ Digimagz/app/src/main/java/com/monicatifanyz/digimagz/api/ApiInterface.kt
@@ -0,0 +1,9 @@
+package com.monicatifanyz.digimagz.api
+
+import retrofit2.http.GET
+
+class ApiInterface {
+
+    @GET("dummy/news")
+
+}
