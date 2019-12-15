9ad88040c982943715ecf18899355c1d7948f07c
Reyhan Firnas <firnasreyhan@gmail.com>
2019-12-15 21:42:42 +0700

    Up

--- Digimagz/app/src/main/java/com/monicatifanyz/digimagz/view/fragment/SearchFragment.kt
+++ Digimagz/app/src/main/java/com/monicatifanyz/digimagz/view/fragment/SearchFragment.kt
@@ -1,27 +1,28 @@
 package com.monicatifanyz.digimagz.view.fragment
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.monicatifanyz.digimagz.R
+import com.monicatifanyz.digimagz.api.InitRetrofit
 import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    lateinit var initRetrofit: InitRetrofit
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_search, container, false)
+        initRetrofit = InitRetrofit()
+        initRetrofit.getNewsFromApi()
+
+        return view
     }
 
     fun scrollUp() {
